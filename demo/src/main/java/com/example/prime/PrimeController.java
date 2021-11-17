d13902894a89c4bc96715f8e6924646de0d82c70
swseighman <scottseighman@gmail.com>
2021-11-16 22:17:16 -0500

    Modified src

--- demo/src/main/java/com/example/prime/PrimeController.java
+++ demo/src/main/java/com/example/prime/PrimeController.java
@@ -3,6 +3,7 @@ package com.example.prime;
 import com.example.prime.service.PrimeNumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import java.util.List;
  *
  * @author Brendt Lucas
  */
+@SpringBootApplication
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/primes")

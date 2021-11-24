1555ab9d7af921070d3b7d88c3795533e41e71c9
kris.foster <me@krismade.me>
2021-11-24 09:34:58 +0000

    Changed the datasource to the network name, started making changes to support building optimised native images

--- demo/build.sh
+++ demo/build.sh
@@ -10,6 +10,11 @@ echo "Building Native Image..."
 mvn package -Pnative
 mvn package -Pnative-g1
 mvn package -Pnative-inst
+if test -f "./profiles/primes.iprof"; then
+    echo "Building OPTIMISED Native Image..."
+    mvn package -Pnative-opt
+    echo "Building OPTIMISED Native Image DONE"
+fi
 echo "Building Native Image - DONE"
 
 echo "Building docker containers..."
@@ -19,4 +24,9 @@ docker build -f ./Dockerfile.graalee -t localhost/primes:graalee.${VER} .
 docker build -f ./Dockerfile.native -t localhost/primes:native.${VER} .
 docker build -f ./Dockerfile.native-g1 -t localhost/primes:nativeg1.${VER} .
 docker build -f ./Dockerfile.native-inst -t localhost/primes:nativeinst.${VER} .
+if test -f "./target/prime-opt"; then
+    echo "Building OPTIMISED docker container for Native Image..."
+    docker build -f ./Dockerfile.native-opt -t localhost/primes:nativeinst.${VER} .
+    echo "Building OPTIMISED docker container for Native Image DONE"
+fi
 echo "Docker docker containers - DONE"
--- demo/stress.sh
+++ demo/stress.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-hey -z 10m http://localhost:8085/primes &
-hey -z 10m http://localhost:8086/primes &
-hey -z 10m http://localhost:8087/primes &
-hey -z 10m http://localhost:8088/primes &
-hey -z 10m http://localhost:8089/primes &
+hey -z 10m http://localhost:8085/primes/9999999999 &
+hey -z 10m http://localhost:8086/primes/9999999999 &
+hey -z 10m http://localhost:8087/primes/9999999999 &
+hey -z 10m http://localhost:8088/primes/9999999999 &
+hey -z 10m http://localhost:8089/primes/9999999999 &

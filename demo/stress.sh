6bb057f8f632b00bb7b3bf48854ea202bc81f3f9
kris.foster <me@krismade.me>
2022-03-11 13:45:39 +0000

    Updated stress.sh

--- demo/stress.sh
+++ demo/stress.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-hey -z 10m http://localhost:8085/primes?upperBound=99999 &
-hey -z 10m http://localhost:8086/primes?upperBound=99999 &
-hey -z 10m http://localhost:8087/primes?upperBound=99999 &
-hey -z 10m http://localhost:8088/primes?upperBound=99999 &
-hey -z 10m http://localhost:8089/primes?upperBound=99999 &
-hey -z 10m http://localhost:8090/primes?upperBound=99999 &
+hey -z 20m http://localhost:8085/primes?upperBound=99999 &
+hey -z 20m http://localhost:8086/primes?upperBound=99999 &
+hey -z 20m http://localhost:8087/primes?upperBound=99999 &
+hey -z 20m http://localhost:8088/primes?upperBound=99999 &
+hey -z 20m http://localhost:8089/primes?upperBound=99999 &
+hey -z 20m http://localhost:8090/primes?upperBound=99999 &
